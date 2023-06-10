6b1976553ad1e5bac58942427f80b98f7615d541
a-voinov <alariht@gmail.com>
2023-06-11 01:19:56 +0700

    https://doc.rust-lang.org/book/ch05-03-method-syntax.html

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,53 @@ struct Rectangle {
     height: u32
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
-        height: 50
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
     };
 
     println!("rect1 is {:#?}", rect1);
     dbg!(&rect1);
 
+    println!("rect2 is {:#?}", rect2);
+    println!("rect3 is {:#?}", rect3);
+
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    )
-}
+        rect1.area()
+    );
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(42);
+    println!("It's hip to be {:#?}", square)
 }
